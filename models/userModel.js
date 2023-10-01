ba620c3c376d94cedb1ac0c02f6b4d03be790b07
Nastia <nastiakopylova1@mail.ru>
2023-10-01 16:15:41 +0300

    Integrate bcrypt password hashing in userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,5 +26,10 @@ const userSchema = new mongoose.Schema({
     default: 'Russia',
   }
 }, { timestamps: true });
+
+userSchema.pre('save', async function() {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 export default mongoose.model('User', userSchema);
