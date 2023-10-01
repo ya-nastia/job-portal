e9b75ab844e5787a6ac58a1fe5db1693d6926f2c
Nastia <nastiakopylova1@mail.ru>
2023-10-01 16:06:48 +0300

    Optimize error handling in registerController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,41 +1,29 @@
 import userModel from "../models/userModel.js";
 
-export const registerController = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+export const registerController = async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    if (!name) {
-      res.status(400).send({ message: 'Please provide name', success: false });
-    }
-
-    if (!email) {
-      res.status(400).send({ message: 'Please provide email', success: false });
-    }
-
-    if (!password) {
-      res.status(400).send({ message: 'Please provide password', success: false });
-    }
-
-    const existingUser = await userModel.findOne({ email });
-    if (existingUser) {
-      return res.status(200).send({
-        success: false,
-        message: 'Email already exists.'
-      })
-    }
-
-    const user = await userModel.create({ name, email, password });
-    res.status(201).send({
-      success: true,
-      massage: 'User created successfully',
-      user
-    })
-  } catch(error) {
-    console.log(error);
-    res.status(400).send({
-      message: 'Error in register controller',
-      success: false,
-      error
-    })
+  if (!name) {
+    next('Name is required');
   }
+
+  if (!email) {
+    next('Email is required');
+  }
+
+  if (!password) {
+    next('Password is required and should be greater than 6 characters');
+  }
+
+  const existingUser = await userModel.findOne({ email });
+  if (existingUser) {
+    next('User already exists.');
+  }
+
+  const user = await userModel.create({ name, email, password });
+  res.status(201).send({
+    success: true,
+    massage: 'User created successfully',
+    user
+  })
 }
