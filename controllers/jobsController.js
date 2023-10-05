c6f7326caa909cf7dc7d71c2a793fd68023830df
Nastia <nastiakopylova1@mail.ru>
2023-10-05 20:20:21 +0300

    implement getAllJobsController and added new route for getting all jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -11,3 +11,12 @@ export const createJobController = async (req, res, next) => {
   const job = await jobsModel.create(req.body);
   res.status(201).json({ job });
 };
+
+export const getAllJobsController = async (req, res, next) => {
+  const jobs = await jobsModel.find({ createdBy: req.user.userId });
+  
+  res.status(200).json({
+    totalJobs: jobs.length,
+    jobs,
+  });
+};
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createJobController } from '../controllers/jobsController.js';
+import { createJobController, getAllJobsController } from '../controllers/jobsController.js';
 import userAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/create-job', userAuth, createJobController);
+
+router.get('/get-jobs', userAuth, getAllJobsController);
 
 export default router;
