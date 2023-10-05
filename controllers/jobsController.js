cc201994ca624eb60e0f71ab68f3ff112528e369
Nastia <nastiakopylova1@mail.ru>
2023-10-05 20:40:50 +0300

    implement updateJobController and added route for job updates

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -14,9 +14,35 @@ export const createJobController = async (req, res, next) => {
 
 export const getAllJobsController = async (req, res, next) => {
   const jobs = await jobsModel.find({ createdBy: req.user.userId });
-  
+
   res.status(200).json({
     totalJobs: jobs.length,
     jobs,
   });
+};
+
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  
+  if (!company || !position) {
+    next('Please provide all fields');
+  }
+  
+  const job = await jobsModel.findOne({ _id: id });
+  
+  if (!job) {
+    next(`No jobs found with this id ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next('You are not authorized to update this job');
+    return;
+  }
+
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  
+  res.status(200).json({ updateJob });
 };
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import { createJobController, getAllJobsController } from '../controllers/jobsController.js';
+import { 
+  createJobController, 
+  getAllJobsController,
+  updateJobController,
+} from '../controllers/jobsController.js';
 import userAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +11,7 @@ const router = express.Router();
 router.post('/create-job', userAuth, createJobController);
 
 router.get('/get-jobs', userAuth, getAllJobsController);
+
+router.patch('/update-job/:id', userAuth, updateJobController);
 
 export default router;
