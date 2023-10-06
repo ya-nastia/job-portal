55178b5e8bd1ce0428faf137c4375247c7fac649
Nastia <nastiakopylova1@mail.ru>
2023-10-06 20:16:38 +0300

    implement pagination in getAllJobsController

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -48,11 +48,22 @@ export const getAllJobsController = async (req, res, next) => {
     queryResult = queryResult.sort('-position');
   }
 
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  queryResult = queryResult.skip(skip).limit(limit);
+
+  const totalJobs = await jobsModel.countDocuments(queryResult);
+  const numOfPage = Math.ceil(totalJobs / limit);
+
   const jobs = await queryResult;
 
   res.status(200).json({
-    totalJobs: jobs.length,
+    totalJobs,
     jobs,
+    numOfPage,
   });
 };
 
