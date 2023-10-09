a5a2b26d926a0289be79e4901363d5eef7c03df5
Nastia <nastiakopylova1@mail.ru>
2023-10-09 22:08:32 +0300

    implement swagger for documenting api

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import 'express-async-errors';
 import helmet from 'helmet';
 import xss from 'xss-clean';
 import mongoSanitize from 'express-mongo-sanitize';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDoc from 'swagger-jsdoc';
 
 import connectDB from './config/db.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
@@ -17,6 +19,24 @@ import jobsRoutes from './routes/jobsRoutes.js';
 dotenv.config();
 
 connectDB();
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Job Portal Application',
+            description: 'Node Express.js Job Portal Application',
+        },
+        servers: [
+            {
+                url: 'http://localhost:8080'
+            }
+        ],
+    },
+    apis: ['./routes/*.js'],
+};
+
+const spec = swaggerDoc(options);
 
 const app = express();
  
@@ -31,6 +51,8 @@ app.use('/api/v1/test', testRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/job', jobsRoutes);
+
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(spec));
 
 app.use(errorMiddleware);
 
