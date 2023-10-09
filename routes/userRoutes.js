09929daefa2dafc3946b36ee6738f1fba52645c7
Nastia <nastiakopylova1@mail.ru>
2023-10-09 22:30:50 +0300

    add swagger documentation to user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,62 @@ import userAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/user/update-user:
+ *   put:
+ *     summary: Update user information
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               location:
+ *                 type: string
+ *             example:
+ *               name: John
+ *               email: johndoe@example.com
+ *               lastName: Doe
+ *               location: Moscow
+ *     responses:
+ *       200:
+ *         description: User information updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *                 token:
+ *                   type: string
+ *             example:
+ *               user:
+ *                 id: GDHJGD788BJBJ
+ *                 name: John
+ *                 lastName: Doe
+ *                 email: johndoe@example.com
+ *                 location: Moscow
+ *               token: <your-auth-token>
+ *       400:
+ *         description: Bad Request - Please provide all required fields
+ *       401:
+ *         description: Unauthorized - Auth Failed
+ *       500:
+ *         description: Internal Server Error
+ */
+
 router.put('/update-user', userAuth, updateUserController);
 
 export default router;
