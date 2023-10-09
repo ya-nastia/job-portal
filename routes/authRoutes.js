1c92aeaeee7b1d6c84a33e185c960619278a2dff
Nastia <nastiakopylova1@mail.ru>
2023-10-09 22:19:00 +0300

    add swagger documentation to auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,101 @@ const limiter = rateLimit({
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - name
+ *        - lastName
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: The auto-generated ID of user collection
+ *          example : DHSASDHJDJHVAJDSVJAVSD
+ *        name:
+ *          type: string
+ *          description: User name
+ *        lastName:
+ *          type: string
+ *          description: User last name
+ *        email:
+ *          type: string
+ *          description: User email address
+ *        password:
+ *          type: string
+ *          description: User password should be greater then 6 characters
+ *        location:
+ *          type: string
+ *          description: User city or country
+ *      example:
+ *        id: GDHJGD788BJBJ
+ *        name: John
+ *        lastName: Doe
+ *        email: johndoe@gmail.com
+ *        password: test@123
+ *        location: Moscow
+ */
+
+/**
+ *  @swagger
+ *  tags:
+ *    name: Auth
+ *    description: authentication apis
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *    post:
+ *      summary: register new user
+ *      tags: [Auth]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *      responses:
+ *        200:
+ *          description: user created successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *        500:
+ *          description: internal server error
+ */
+
 router.post('/register', limiter, registerController);
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *  post:
+ *    summary: login page
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: login successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      500:
+ *        description: something went wrong
+ */
+
 router.post('/login', limiter, loginController)
 
 export default router;
