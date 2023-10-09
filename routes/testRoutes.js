b2be7e2b721a51302cf35df5354e2d03208a9061
Nastia <nastiakopylova1@mail.ru>
2023-10-09 22:25:24 +0300

    add swagger documentation to test route

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -4,6 +4,39 @@ import userAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/test/test-post:
+ *   post:
+ *     summary: Test Post Route
+ *     tags: [Test]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:
+ *               name: John
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *             example:
+ *               message: "Your name is John"
+ *       500:
+ *         description: Internal Server Error
+ */
+
 router.post('/test-post', userAuth, testPostController);
 
 export default router;
