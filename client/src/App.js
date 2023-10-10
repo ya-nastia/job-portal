e90afff16a62ec98adbda39e74de5702dd3abdea
Nastia <nastiakopylova1@mail.ru>
2023-10-10 22:47:05 +0300

    define routes in the app.js using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
