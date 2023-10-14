b3cfda6cc8cd91c1a8cbd4065863763d54900f26
Nastia <nastiakopylova1@mail.ru>
2023-10-14 11:01:42 +0300

    fixed authSlice import in store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { alertSlice } from "./features/alertSlice";
+import { authSlice } from "./features/auth/authSlice";
 
 export default configureStore({
   reducer: {
