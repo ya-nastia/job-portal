aa3bc24cf378f0332f7f4c3775067aec00363f9d
Nastia <nastiakopylova1@mail.ru>
2023-10-14 11:47:15 +0300

    fixed spinner styles

--- client/src/components/Spinner/Spinner.jsx
+++ client/src/components/Spinner/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="d-flex justify-content-center" style={{ marginTop: "50%" }}>
+    <div className="d-flex justify-content-center" style={{ marginTop: "20%" }}>
       <div className="spinner-border" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
